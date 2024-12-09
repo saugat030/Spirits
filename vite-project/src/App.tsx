69ae6d32eac5cbdffce5df90de4278e514d7bdf9
Saugat Dahal <dahalsaugat@gmail.com>
2024-12-09 19:52:59 +0545

    fixed a typo

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import NavBar from "./Components/NavBar";
 import Landing from "./Components/Landing";
-import WhyUs from "./Components/WhyUS";
 import BestSelling from "./Components/BestSelling";
 import Years from "./Components/Years";
 import Footer from "./Components/Footer";
+import WhyUs from "./Components/WhyUs";
 function App() {
   return (
     <div className="font-Poppins">
