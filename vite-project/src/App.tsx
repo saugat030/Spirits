a5fa2a4f3341485e1cc723ef8524aa123ee541d4
ZuluMumu <saugatdahal030@gmail.com>
2024-12-16 21:10:37 +0545

    added react-router-dom

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,20 +1,15 @@
 import "./App.css";
-import NavBar from "./Components/NavBar";
-import Landing from "./Components/Landing";
-import BestSelling from "./Components/BestSelling";
-import Years from "./Components/Years";
-import Footer from "./Components/Footer";
-import WhyUs from "./Components/WhyUS";
+import ProductsPage from "./pages/ProductsPage";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="font-Poppins">
-      <NavBar />
-      <Landing></Landing>
-      <WhyUs />
-      <BestSelling />
-      <Years />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ vite-project/src/pages/ProductsPage.tsx
@@ -0,0 +1,11 @@
+import NavBar from "../Components/NavBar";
+const ProductsPage = () => {
+  return (
+    <div className="bg-slate-600 h-screen">
+      <NavBar />
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default ProductsPage;
