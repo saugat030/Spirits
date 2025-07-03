bb11b442c618f6ac5c1491907bfb57af14942bd4
ZuluMumu <saugatdahal030@gmail.com>
2025-07-03 14:56:58 +0545

    CORS error

--- server/server.js
+++ server/server.js
@@ -18,7 +18,11 @@ app.use(express.json());
 //VVI for running projs in different port react ko arkai port ra express ko arkai port huda kam lagne.
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
+    origin: [
+      "http://localhost:5174",
+      "http://localhost:5173",
+      "https://spirits-clkh.vercel.app",
+    ],
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
     credentials: true,
