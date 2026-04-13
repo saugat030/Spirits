40898a70eb9e74f25c315c4b38c824ab47545b96
ZuluMumu <saugatdahal030@gmail.com>
2026-04-13 10:29:19 +0545

    change product types

--- spirits-fe/src/types/api.types.ts
+++ spirits-fe/src/types/api.types.ts
@@ -18,14 +18,40 @@ export type Filters = {
   maxPrice: number | null;
 }
 
-export type Product = {
+export interface ImageType {
+  url: string;
+  alt_text: string;
+}
+
+export interface Promotion {
   id: string;
   name: string;
-  sku?: string;
-  imageLink: string;
-  description: string;
-  quantity: number;
-  typeId: number;
-  typeName: string;
+  discountType: "percentage" | "fixed";
+  discountValue: number;
+  endDate: string;
+}
+
+
+export interface ProductVariant {
+  liquorId: string;
+  id: string;
+  size: string;
+  sku: string;
   price: number;
-};
+  inventoryQuantity: number;
+  variantImage: ImageType;
+  promotions: Promotion[];
+  discountedPrice: number;
+}
+
+export interface Product {
+  id: string;
+  name: string;
+  thumbnail_url: string;
+  images: ImageType[];
+  description: string;
+  categoryId: string;
+  categoryName: string;
+  variants: ProductVariant[];
+  minDiscountedPrice: number;
+}
